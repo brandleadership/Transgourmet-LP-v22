8571ae7e22a3812a381e08b6f1bd51694c7489ed
Iryna-Alchynska <106669156+Iryna-Alchynska@users.noreply.github.com>
2023-09-05 13:08:01 +0200

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,3 @@
-/*const path = require('path');
-
-const {BuildConfig, ModuleConfig, WebpackConfigBuilder, Version, DesignType} = require('@bsi-cx/design-build');
-
-module.exports = WebpackConfigBuilder.fromConfigs(
-  new BuildConfig()
-    .withName('email')
-    .withVersion('1.0.0')
-    .withDesignType(DesignType.EMAIL)
-    .withTargetVersion(Version.CX_1_3)
-    .withRootPath(path.resolve(__dirname, 'templates', 'email'))
-    .withPropertiesFilePath('properties.js'),
-  new BuildConfig()
-    .withName('landingpage')
-    .withVersion('1.0.0')
-    .withDesignType(DesignType.LANDINGPAGE)
-    .withTargetVersion(Version.CX_1_3)
-    .withRootPath(path.resolve(__dirname, 'templates', 'landingpage'))
-    .withPropertiesFilePath('properties.js')
-    .withModules(
-      new ModuleConfig()
-        .withName('main')
-        .withPath('main.js')),
-  new BuildConfig()
-    .withName('website')
-    .withVersion('1.0.0')
-    .withDesignType(DesignType.WEBSITE)
-    .withTargetVersion(Version.CX_1_3)
-    .withRootPath(path.resolve(__dirname, 'templates', 'website'))
-    .withPropertiesFilePath('properties.js')
-    .withModules(
-      new ModuleConfig()
-        .withName('main')
-        .withPath('main.js')));*/
-
 const path = require("path");
 
 const {
@@ -60,9 +25,9 @@ module.exports = WebpackConfigBuilder.fromConfigs(
   landingpageBuildConfig
     .clone()
     .withName("Transgourmet-LP-de-cx-22.0")
+    .withPropertiesFilePath("properties.js"),
+  landingpageBuildConfig
+    .clone()
+    .withName("Transgourmet-LP-en-cx-22.0")
     .withPropertiesFilePath("properties.js")
-  // landingpageBuildConfig
-  //   .clone()
-  //   .withName("Transgourmet-LP-en-cx-22.0")
-  //   .withPropertiesFilePath("properties.js")
 );
